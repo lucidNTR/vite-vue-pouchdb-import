0592e9c9849ca1b4ce161b548772c5cd0f510d43
lucidNTR <git@stackblitz.com>
2025-01-11 16:29:06 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+
+import Pouch from 'pouchdb-browser';
+
+createApp(App).mount('#app');
+
+// const db = new PouchDB('kittens');
